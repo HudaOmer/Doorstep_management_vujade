f90a173be121dd9ccd99b3bbfa76d88c76f0757e
HudaOmer <hudaoe175@gmail.com>
2024-10-23 09:48:03 +0200

    Fix utils

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -5,3 +5,6 @@ Color contrastColor = const Color.fromARGB(255, 66, 66, 74);
 Color contrastColorTrans = const Color.fromARGB(255, 127, 127, 144);
 Color greyColor = const Color.fromARGB(38, 217, 217, 217);
 Color mediumGreyColor = const Color.fromARGB(255, 226, 226, 226);
+
+TextStyle mainTitle = const TextStyle(
+    color: Colors.black, fontSize: 20, fontWeight: FontWeight.w700);
